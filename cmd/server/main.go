74244f5c79d24a50c03e0beff380736ba3ead183
rachana <rachanachikka98@gmail.com>
2022-07-21 10:17:31 +0530

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/rachanachikka98/go-rest-api-course/internal/transport/http"
+)
+
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Setting Up Our APP")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	fmt.Println("Go REST API Course")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rachanachikka98/go-rest-api-course
+
+go 1.18
